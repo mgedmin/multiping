4b091efa8cbf0b9f65f9a0451a9c9e22dc96073c
Marius Gedminas <marius@gedmin.as>
2020-05-22 13:00:54 +0300

    Some unit tests for UI drawing

--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@ import curses
 import os
 import sys
 import time
+from collections import defaultdict
 
 import mock
 import pytest
@@ -10,11 +11,18 @@ import multiping
 
 
 class FakePinger:
-    def __init__(self):
-        self.results = {}
+    def __init__(self, hour=12, minute=30, second=0):
+        self.results = defaultdict(lambda: ' ')
+        self.version = 0
+        self.interval = 1
+        self.status = self.results
+        self.sent = self.received = 0
+        self.started = time.mktime(
+            (2000, 5, 22, hour, minute, second, 0, 0, 0))
 
     def set(self, idx, result):
         self.results[idx] = result
+        self.version += 1
 
 
 def test_Ping():
@@ -104,6 +112,132 @@ def test_Pinger_queue(monkeypatch):
     monkeypatch.setattr(multiping, 'sleep', lambda seconds: None)
     pinger = LimitedPinger('localhost', 1)
     pinger.run()
+
+
+class FakeCursesWindow:
+
+    def __init__(self, width=80, height=24):
+        self._screen = [[' '] * width for _ in range(height)]
+        self._row = 0
+        self._col = 0
+        self._width = width
+        self._height = height
+
+    def addstr(self, *args):
+        # args may be (row, col, text), or (text, attr), or just (text, )
+        if len(args) == 3:
+            self._row, self._col, text = args
+        elif len(args) == 2:
+            text, attr = args
+        else:
+            text = args[0]
+        try:
+            for c in text:
+                self._screen[self._row][self._col] = c
+                self._col += 1
+                # it is a curses.error to start drawing outside the screen,
+                # but too long strings get truncated
+                if self._col >= self._width:
+                    break
+        except IndexError:
+            raise curses.error
+
+    def move(self, row, col):
+        self._row = row
+        self._col = col
+
+    def clrtoeol(self):
+        try:
+            for col in range(self._col, self._width):
+                self._screen[self._row][col] = ' '
+        except IndexError:
+            raise curses.error
+
+    def clrtobot(self):
+        for row in range(self._row, self._height):
+            for col in range(self._col, self._width):
+                self._screen[row][col] = ' '
+
+    def _lines(self):
+        return [
+            ''.join(row).rstrip() for row in self._screen
+        ]
+
+    def _text(self):
+        return '\n'.join(self._lines())
+
+
+@pytest.fixture
+def fake_curses(monkeypatch):
+    monkeypatch.setattr(curses, 'use_default_colors', lambda: None)
+    monkeypatch.setattr(curses, 'init_pair', lambda pair, fg, bg: None)
+    monkeypatch.setattr(curses, 'curs_set', lambda visibility: None)
+    monkeypatch.setattr(curses, 'color_pair', lambda pair: pair)
+
+
+def test_UI_draw(fake_curses):
+    pinger = FakePinger(hour=12, minute=30, second=3)
+    pinger.set(0, '#')
+    pinger.set(31, '%')
+    pinger.set(62, '-')
+    pinger.set(93, '.')
+    win = FakeCursesWindow(width=80, height=7)
+    ui = multiping.UI(win, 2, 2, 30, 5, pinger, 'example.com')
+    ui.draw()
+    assert win._text() == '\n'.join([
+        '',
+        '  pinging example.com',
+        '  12:30 [   #                          ]',
+        '  12:30 [    %                         ]',
+        '  12:31 [     -                        ]',
+        '  12:31 [      .                       ]',
+        '',
+    ])
+
+
+def test_UI_draw_screen_too_small(fake_curses):
+    pinger = FakePinger(hour=12, minute=30, second=3)
+    pinger.set(0, '#')
+    pinger.set(31, '%')
+    pinger.set(62, '-')
+    pinger.set(93, '.')
+    win = FakeCursesWindow(width=20, height=1)
+    ui = multiping.UI(win, 1, 2, 30, 4, pinger, 'example.com')
+    ui.draw()
+    assert win._text() == '\n'.join([
+        '  pinging example.co',
+    ])
+
+
+def test_UI_draw_packet_loss(fake_curses):
+    pinger = FakePinger()
+    pinger.sent = 3
+    pinger.received = 2
+    win = FakeCursesWindow(height=1)
+    ui = multiping.UI(win, 1, 0, 60, 0, pinger, 'example.com')
+    ui.draw()
+    assert win._text() == '\n'.join([
+        'pinging example.com: packet loss 33.3%',
+    ])
+
+
+def test_UI_draw_autoscroll(fake_curses):
+    pinger = FakePinger(hour=12, minute=30, second=3)
+    pinger.set(0, '#')
+    pinger.set(31, '%')
+    pinger.set(62, '-')
+    pinger.set(93, '.')
+    win = FakeCursesWindow(width=80, height=6)
+    ui = multiping.UI(win, 1, 2, 30, 5, pinger, 'example.com')
+    ui.draw()
+    assert win._text() == '\n'.join([
+        '  pinging example.com',
+        '  12:30 [   #                          ]',
+        '  12:30 [    %                         ]',
+        '  12:31 [     -                        ]',
+        '  12:31 [      .                       ]',
+        '',
+    ])
 
 
 @pytest.mark.parametrize('argv', [
