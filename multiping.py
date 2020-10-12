ab931439a0de4300eab90f2fb10cc518813aa975
Marius Gedminas <marius@gedmin.as>
2020-10-12 11:56:48 +0300

    Back to development: 1.3.1

--- multiping.py
+++ multiping.py
@@ -35,7 +35,7 @@ from threading import Thread
 from time import localtime, sleep, strftime, time
 
 
-__version__ = '1.3.0'
+__version__ = '1.3.1.dev0'
 __author__ = 'Marius Gedminas <marius@gedmin.as>'
 __url__ = 'https://github.com/mgedmin/scripts/blob/master/multiping.py'
 __licence__ = 'GPL v2 or later'
